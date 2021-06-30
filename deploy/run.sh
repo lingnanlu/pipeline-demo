70d9b0c47f5c1b7a2c9af5fbfd5b8f1a0eb1c2ae
elder.han <lingnanlu@gmail.com>
2021-06-30 15:03:32 +0800

    dev

--- deploy/run.sh
+++ deploy/run.sh
@@ -6,4 +6,4 @@ ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}' | xargs kill -9
 # 休息5秒，等待之前的实例关闭
 sleep 2
 #
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=10001 -jar "target/$JARFILE"  > log.txt &
+nohup java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=10001 -jar "target/$JARFILE"  > log.txt &
