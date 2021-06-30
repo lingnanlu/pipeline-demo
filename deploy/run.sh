0750175cd106d972f5148bae3e95a0f36b49e627
elder.han <lingnanlu@gmail.com>
2021-06-30 14:49:44 +0800

    dev

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,7 +3,7 @@ JARFILE="pipeline-demo-1.0-SNAPSHOT.jar"
 # 杀掉之前的
 ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}' | xargs kill -9
 
-## 休息5秒，等待之前的实例关闭
-#sleep 5
+# 休息5秒，等待之前的实例关闭
+sleep 2
 #
 #java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=10001 -jar "target/$JARFILE"  > log.txt &
